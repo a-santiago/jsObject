2e66e810f0517b58bc706fdd297bc62b2d1eecff
a-santiago <santiago.62596@gmail.com>
2021-10-07 16:06:18 -0400

    commit_001
    Object Address book has been completed

--- js/site.js
+++ js/site.js
@@ -1,9 +1,42 @@
+let contacts = []
 
-//Displays a message to the User
-function displayMessage() {
-    let msg = "";
-    msg =document.getElementById("message").value;
-    Swal.fire(msg);
-    
+//Add Contact to Form
+function addContact() {
+
+    // Retrieve Items or Values
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let email = document.getElementById("email").value;
+
+    //Create an Object with the values from the form
+    const contact = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email
+    };
+
+    // Set Items
+    contact.firstName = firstName;
+    contact.lastName = lastName;
+    contact.email = email;
+
+    //Push Values to Array
+    contacts.push(contact);
+
+    //call display contacts
+    displayContacts(contacts);
+
+    //clear the form
+    document.getElementById("contactForm").reset();
 }
 
+
+function displayContacts(contacts) {
+    document.getElementById("results").innerHTML = "";
+    for (let i = 0; i < contacts.length; i++) {
+
+        let TemplateRow = `<tr><td>${contacts[i].firstName}</td><td>${contacts[i].lastName}</td><td>${contacts[i].email}</td></tr>`
+        document.getElementById("results").innerHTML += TemplateRow;
+    }
+
+}
